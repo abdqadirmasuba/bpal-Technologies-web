1d1b52d4b3b48beb97ebf8d0c07b8cec48f8469d
abdqadir <masubaabdq@gmail.com>
2025-08-11 07:10:51 +0300

    paths

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/bpal-Technologies-web">
+        <BrowserRouter basename="/">
           <div className="flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/bpal-Technologies-web/",
+  base:"/",
   server: {
     host: "::",
     port: 8080,
